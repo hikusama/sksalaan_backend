9b5c13e76c8c2200e0c8b4ad2b8c3311f5113a77
Hikusama <leomessi25k@gmail.com>
2025-06-24 22:34:12 +0800

    Job support model configured

--- app/Models/job_support.php
+++ app/Models/job_support.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Job_support extends Model
 {
     //
+    protected $fillable = [
+        'youth_user_id',
+        'task',
+        'paid_at',
+        'location',
+        'start',
+        'end',
+    ];
+
+    public function yUser()
+    {
+        return $this->belongsTo(YouthUser::class);
+    }
 }
